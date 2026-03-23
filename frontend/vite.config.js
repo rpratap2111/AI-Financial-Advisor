036191401f5dbc1c4c56e3417a92e0ef55fdbe18
rpratap2111 <rpratap2111@gmail.com>
2026-03-23 19:32:32 +0530

    simple change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('framer-motion')) return 'framer-motion';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
